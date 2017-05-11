329af813d58e38e0d154d82d0730fdf802817503
Yoshiya Hinosawa <stibium121@gmail.com>
2017-05-11 10:30:52 +0900

    refactor: minor refactoring

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ const getSource = (file, mdSources) => {
   }
 }
 
-const sortFiles = mdSources => through2.obj((file, enc, cb) => {
+/**
+ * Sorts files and creates file mapping according to alias prop in frontmatters.
+ * @return {Transform}
+ */
+const sortFiles = () => through2.obj((file, enc, cb) => {
   file.files = file.files.slice(0).sort((x, y) => {
     if (x.data.index !== y.data.index) {
       return x.data.index - y.data.index
@@ -132,16 +136,14 @@ module.exports = (bulbo, options) => {
   })
 
   pipeline
-    .pipe(gulpdata(file => {
-      return getSource(file, mdSources)
-    }))
+    .pipe(gulpdata(file => getSource(file, mdSources)))
     .pipe(frontMatter({ property: 'fm' }))
     .pipe(marked())
     .pipe(branch.obj(src => [
       src.pipe(accumulate(paths.output.index, { debounce: true })),
       src.pipe(accumulate.through({ debounce: true }))
     ]))
-    .pipe(sortFiles(mdSources))
+    .pipe(sortFiles())
     .pipe(layout1.nunjucks(file => (
       file.relative === paths.output.index ? paths.layout.index : paths.layout.page
     ), { data }))
