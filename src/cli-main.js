304b0fdc8b0ca3525be28c235160c0618c21a24c
Yoshiya Hinosawa <stibium121@gmail.com>
2017-05-19 15:20:20 +0900

    refactor: switch to action-selector

--- src/cli-main.js
+++ src/cli-main.js
@@ -1,20 +1,51 @@
 const chalk = require('chalk')
 const minirocket = require('minirocket')
+const { select } = require('action-selector')
 
-module.exports = argv => {
-  const v = argv.v
-  const h = argv.h
-  const version = argv.version
-  const help = argv.help
-  const action = argv._[0]
+module.exports = argv => new Cli(argv).main()
 
-  minirocket({
-    version: v || version,
-    help: h || help,
-    serve: !action,
-    [action]: true
-  }, action => action(argv)).on('no-action', name => {
+class Cli {
+  constructor (argv) {
+    this.argv = argv
+  }
+
+  main () {
+    const argv = this.argv
+
+    const v = argv.v
+    const h = argv.h
+    const version = argv.version
+    const help = argv.help
+    const action = argv._[0]
+
+    select(this, {
+      version: v || version,
+      help: h || help,
+      serve: !action,
+      [action]: true
+    })
+    .on('action', action => action.call(this))
+    .on('no-action', name => this.noAction(name))
+  }
+
+  noAction (name) {
     console.log(chalk.red('Error:'), `'${name}' is not a domaindoc command. See 'domaindoc --help'.`)
     process.exit(1)
-  })
+  }
+
+  'action:version' () {
+    require('./actions/version')()
+  }
+
+  'action:help' () {
+    require('./actions/help')()
+  }
+
+  'action:build' () {
+    require('./actions/build')()
+  }
+
+  'action:serve' () {
+    require('./actions/serve')()
+  }
 }
